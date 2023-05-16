bd34ede94291cc004c9bac82d25943d0d0347e74
AgentOfTheOreos <93847573+AgentOfTheOreos@users.noreply.github.com>
2023-05-16 22:06:43 +0300

    Add files via upload

--- AmazonBookStore/src/Book.java
+++ AmazonBookStore/src/Book.java
@@ -1,38 +1,101 @@
-import java.util.*;
-import java.io.*;
+import java.util.Objects;
 
 class Book
 {
     private String title;
     private String author;
     private String isbn;
+    private String genre;
     private double price;
+    private Date releaseDate;
 
-    public Book(String title, String author, String isbn, double price)
+    public Book(String title, String author, String isbn, String genre, double price, Date releaseDate)
     {
         this.title = title;
         this.author = author;
         this.isbn = isbn;
+        this.genre = genre;
         this.price = price;
+        this.releaseDate = releaseDate;
     }
 
-
-
-    public String getTitle()
-    {
+    public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getAuthor() {
         return author;
     }
 
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
     public String getIsbn() {
         return isbn;
     }
 
-    public double getPrice()
-    {
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public double getPrice() {
         return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    // Extra Methods
+    public void displayDetails() {
+        System.out.println(this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", genre='" + genre + '\'' +
+                ", price=" + price + '$' +
+                ", releaseDate=" + releaseDate +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(book.getPrice(), price) && Objects.equals(title, book.getTitle()) &&
+                Objects.equals(author, book.getAuthor()) && Objects.equals(isbn, book.getIsbn()) &&
+                Objects.equals(genre, book.getGenre()) && Objects.equals(releaseDate, book.getReleaseDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, isbn, genre, price, releaseDate);
     }
 }
--- AmazonBookStore/src/User.java
+++ AmazonBookStore/src/User.java
@@ -1,17 +1,23 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 class User
 {
     private String username;
+    private Address address;
     private String password;
-    private List<Book> purchaseHistory;
+    private String email;
+    private List<Book> purchasedBooks;
+    private List<Book> basket;
 
-    public User(String username, String password)
-    {
+    public User(String username, Address address, String password, String email,
+                List<Book> purchasedBooks, List<Book> basket) {
         this.username = username;
+        this.address = address;
         this.password = password;
-        this.purchaseHistory = new ArrayList<>();
+        this.email = email;
+        this.purchasedBooks = purchasedBooks;
+        this.basket = basket;
     }
 
 
@@ -19,8 +25,93 @@ class User
         return username;
     }
 
-    public String getPassword()
-    {
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Book> getPurchasedBooks() {
+        return purchasedBooks;
+    }
+
+    public void setPurchasedBooks(List<Book> purchasedBooks) {
+        this.purchasedBooks = purchasedBooks;
+    }
+
+    public List<Book> getBasket() {
+        return basket;
+    }
+
+    public void setBasket(List<Book> basket) {
+        this.basket = basket;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Extra methods
+    public void displayDetails() {
+        System.out.println("User Details:");
+        System.out.println(toString());
+
+        System.out.println("Purchased Books:");
+        for (Book book : purchasedBooks) {
+            System.out.println(book.toString());
+        }
+
+        System.out.println("Books in Basket:");
+        for (Book book : basket) {
+            System.out.println(book.toString());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", address=" + address +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", purchaseHistory=" + purchasedBooks +
+                ", basket=" + basket +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.getUsername())
+                && Objects.equals(address, user.getAddress())
+                && Objects.equals(password, user.getPassword())
+                && Objects.equals(email, user.getEmail())
+                && Objects.equals(purchasedBooks, user.getPurchasedBooks())
+                && Objects.equals(basket, user.getBasket());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, address, password, email, purchasedBooks, basket);
     }
 }
