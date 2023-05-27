dd3c2c5e86fe8c7eae437a4bb72412de7a9bc143
AgentOfTheOreos <93847573+AgentOfTheOreos@users.noreply.github.com>
2023-05-27 03:06:49 +0300

    Add files via upload

---
+++ AmazonBookStore/src/CredentialValidation/PasswordValidator.java
@@ -0,0 +1,14 @@
+package CredentialValidation;
+import java.util.regex.Pattern;
+
+public class PasswordValidator {
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 100;
+    private static final String SPECIAL_CHARACTERS_REGEX = "[!@#$%^&*()-+=]";
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile(
+            "^(?=.*" + SPECIAL_CHARACTERS_REGEX + ").{" + MIN_LENGTH + "," + MAX_LENGTH + "}$"
+    );
+    public static boolean isValid(String password) {
+        return PASSWORD_PATTERN.matcher(password).matches();
+    }
+}
