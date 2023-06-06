e0ea21def050365acf567c0f11913809f3aaea62
AgentOfTheOreos <93847573+AgentOfTheOreos@users.noreply.github.com>
2023-06-06 11:21:02 +0300

    Delete AmazonBookStore directory

--- AmazonBookStore/src/AttributeAccess.java
+++
@@ -1,8 +0,0 @@
-
-public class AttributeAccess
-{
-    public static boolean validatePassword(User user, String currentPassword)
-    {
-        return currentPassword.equals(user.getPassword());
-    }
-}
--- AmazonBookStore/src/CredentialValidation/ISBNValidator.java
+++
@@ -1,8 +0,0 @@
-package CredentialValidation;
-
-public class ISBNValidator {
-    private static final int ISBN_LENGTH = 13;
-    public static boolean isValid(String isbn) {
-        return isbn != null && isbn.length() == ISBN_LENGTH && isbn.matches("\\d+");
-    }
-}
