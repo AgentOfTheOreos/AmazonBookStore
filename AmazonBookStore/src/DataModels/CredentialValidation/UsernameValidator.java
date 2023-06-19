b39305890d858c72c4314fd06851f2707044641e
AgentOfTheOreos <93847573+AgentOfTheOreos@users.noreply.github.com>
2023-06-19 12:52:55 +0300

    Delete AmazonBookStore directory

--- AmazonBookStore/src/DataModels/CredentialValidation/UsernameValidator.java
+++
@@ -1,9 +0,0 @@
-package DataModels.CredentialValidation;
-public class UsernameValidator {
-    private static final int MAX_LENGTH = 25;
-    private static final int MIN_LENGTH = 1;
-    public static boolean isValid(String username) {
-        return username != null && username.length() <= MAX_LENGTH
-                && username.length() >= MIN_LENGTH && !username.contains(" ");
-    }
-}
